9e74c644a911ca18928fd69e486f30e79afa4470
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2020-05-06 15:48:28 +0900

    add: number guess script

---
+++ Assets/Scripts/NumberGuess.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberGuess : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+        max = max + 1;
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+    }
+}
